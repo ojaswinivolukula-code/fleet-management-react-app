bdc35190b0dd2a6483f3e14159ebf6f36dc4d717
Ojaswini <ojaswini.volukula@gmail.com>
2025-12-28 20:02:29 +0530

    Fleetcard

--- src/Components/FleetCard.jsx
+++ src/Components/FleetCard.jsx
@@ -11,8 +11,17 @@ const FleetCard=memo(function FleetCard({
 
 }){
     return(
-        <div>
-            <p>Vehcile Regno:</p>
+        <div style={{border:"1px solid gray" , padding:"15px"}}>
+            <img src="https://stimg.cardekho.com/images/carexteriorimages/630x420/Jaguar/F-Pace/10644/1755774688332/front-left-side-47.jpg?impolicy=resize&imwidth=480" width="100px"/>
+
+           <p><b>Vehcile Regno:</b>{fleet.vehcileRegNo}</p> 
+           <p><b>Category:</b>{fleet.category}</p> 
+           <p><b>Driver Name:</b>{fleet.driverName}</p> 
+           <p><b>Status:</b>{fleet.status}</p>
+           <button onClick={()=>onUpdateDriver(fleet.id)}>Update Driver</button> 
+           <button onClick={()=>onChangeAvailability(fleet.id)}>Change Availability</button>
+            <button onClick={()=>onDelete(fleet.id)}>Delete</button>
+
         </div>
     )
 }

b00292596c5d921b3553ffdde8f1e19fef9be19d
Ojaswini <ojaswini.volukula@gmail.com>
2025-12-28 19:49:45 +0530

    Implementing Admindashboard 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
 import LoginPage from "./Components/LoginPage"
+import { BrowserRouter, Route,Routes } from "react-router-dom"
+import AdminDashboard from "./Components/AdminDashboard"
+
+
 
 function App(){
+  const [isAuth,setIsAuth]=useState(false)
 return(
-  <div>
-    <LoginPage/>
-  </div>
+  <BrowserRouter>
+  <Routes>
+<Route path="./login" element={<Login setIsAuth={setIsAuth}/>}/>
+<Route path="./admin" element={<ProtectedRoute isAuth={isAuth}>
+  <AdminDashboard/>
+</ProtectedRoute>}/>
+<Route path="*" element={<Navigate to="./login"/>}/>
+
+  </Routes>
+  
+  </BrowserRouter>
 )
 }
 export default App
